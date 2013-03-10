4b1e53e5ce80ecfdd6f81d38c93e68a3deb59989
Sven (Sbte) <svenb.linux@gmail.com>
2013-03-10 11:59:49 +0100

    Improve detection of whether we have to interpolate
    Fixes angle interpolation in a nicer way and also fixed case where a turn
    started before anything was rendered

--- source/simulation2/components/CCmpPosition.cpp
+++ source/simulation2/components/CCmpPosition.cpp
@@ -72,7 +72,7 @@ public:
 	entity_pos_t m_YOffset;
 	bool m_RelativeToGround; // whether m_YOffset is relative to terrain/water plane, or an absolute height
 
-	entity_angle_t m_RotX, m_RotY, m_RotZ;
+	entity_angle_t m_RotX, m_RotY, m_LastRotY, m_PrevRotY, m_RotZ;
 	float m_InterpolatedRotY; // not serialized
 
 	static std::string GetSchema()
@@ -121,10 +121,11 @@ public:
 
 		m_RotYSpeed = paramNode.GetChild("TurnRate").ToFixed().ToFloat();
 
-		m_RotX = m_RotY = m_RotZ = entity_angle_t::FromInt(0);
+		m_RotX = m_RotY = m_RotZ = m_PrevRotY = m_LastRotY = entity_angle_t::FromInt(0);
 		m_InterpolatedRotY = 0;
 
 		m_PositionChanged = false;
+		m_Interpolated = false;
 	}
 
 	virtual void Deinit()
@@ -329,6 +330,8 @@ public:
 	virtual void SetYRotation(entity_angle_t y)
 	{
 		m_RotY = y;
+		m_PrevRotY = y;
+		m_LastRotY = y;
 		m_InterpolatedRotY = m_RotY.ToFloat();
 
 		AdvertisePositionChanges();
@@ -420,6 +423,10 @@ public:
 		{
 		case MT_Interpolate:
 		{
+			m_Interpolated = true;
+			if (!m_PositionChanged)
+				return;
+
 			const CMessageInterpolate& msgData = static_cast<const CMessageInterpolate&> (msg);
 
 			float rotY = m_RotY.ToFloat();
@@ -441,11 +448,14 @@ public:
 			// Store the positions from the turn before
 			m_PrevX = m_LastX;
 			m_PrevZ = m_LastZ;
-			
+			m_PrevRotY = m_LastRotY;
+
 			m_LastX = m_X;
 			m_LastZ = m_Z;
+			m_LastRotY = m_RotY;
 
-			m_PositionChanged = m_X != m_PrevX || m_Z != m_PrevZ;
+			m_PositionChanged = m_X != m_PrevX || m_Z != m_PrevZ || m_RotY != m_PrevRotY || !m_Interpolated;
+			m_Interpolated = false;
 
 			break;
 		}
@@ -474,6 +484,7 @@ private:
 	}
 
 	bool m_PositionChanged;
+	bool m_Interpolated;
 };
 
 REGISTER_COMPONENT_TYPE(Position)
